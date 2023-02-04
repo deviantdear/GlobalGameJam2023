0abd81f93b509fd5865ba1628050cec2cf01bfd0
Valarie Adams <deviantdear@gmail.com>
2023-02-04 15:03:19 -0700

    Art, Basic Interactable scripts, Basic follow script, basic health bar script, basic player scripts
    Added a few more folders with useful starter scripts and added a pixel perfect camera to my test scene, ppu 16.
    Added a basic terrain + pickup spawner script

---
+++ GGJ2023/Assets/Scripts/Interactables/ItemPickup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+    public override void Interact()
+    {
+        base.Interact();
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking Up " + item.name);
+
+
+        //Add item to inventory or update UI, do callback
+
+        bool wasPickedUp = true;
+        if (wasPickedUp)
+        {        
+            //Play Sound
+            //heal Player
+
+            Destroy(gameObject);
+        }
+    }
+}
