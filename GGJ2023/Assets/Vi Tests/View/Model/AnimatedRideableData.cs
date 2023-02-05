04c86cc803b6f336d1b6ddf6fb87a9944a7f9043
Vi <1724913+vprime@users.noreply.github.com>
2023-02-05 13:17:33 -0700

    Adding data files, and movement controllers.

--- GGJ2023/Assets/Vi Tests/View/Model/AnimatedRideableData.cs
+++ GGJ2023/Assets/Vi Tests/View/Model/AnimatedRideableData.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace Character
 {
+    [CreateAssetMenu(menuName = "Character/AnimatedRideableData")]
     public class AnimatedRideableData : AnimatedCharacterData
     {
         
