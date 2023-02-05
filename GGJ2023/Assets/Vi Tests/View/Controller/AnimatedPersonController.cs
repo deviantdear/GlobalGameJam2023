6b04c0c63e94633492ead1d7eeed15eaf368567b
Vi <1724913+vprime@users.noreply.github.com>
2023-02-05 14:29:50 -0700

    Adding Cowboy and Player prefabs,

--- GGJ2023/Assets/Vi Tests/View/Controller/AnimatedPersonController.cs
+++ GGJ2023/Assets/Vi Tests/View/Controller/AnimatedPersonController.cs
@@ -6,28 +6,28 @@ namespace Character
 {
     public class AnimatedPersonController : AnimatedCharacterController
     {
-        [SerializeField] AnimatedPersonData m_Data;
+        [SerializeField] AnimatedPersonData m_PersonData;
         [SerializeField] SpriteRenderer m_FaceRenderer;
         [SerializeField] SpriteRenderer m_ArmRenderer;
 
         AnimatedPersonData.FaceState m_FaceState;
         AnimatedPersonData.ArmState m_ArmState;
         
-        internal override AnimatedCharacterData Data { get => m_Data; set => m_Data = (AnimatedPersonData)value; }
+        internal override AnimatedCharacterData Data { get => m_PersonData; set => m_PersonData = (AnimatedPersonData)value; }
 
         internal override void UpdateRenderers(int frame)
         {
             base.UpdateRenderers(frame);
-            var faceStateAnimated = m_Data.face[m_FaceState];
+            var faceStateAnimated = m_PersonData.face[m_FaceState];
             m_FaceRenderer.sprite = faceStateAnimated[(int)(frame % faceStateAnimated.Count)];
             m_FaceRenderer.flipX = (m_ReverseFlip)?  m_Direction.x < 0 : m_Direction.x > 0;
-            var armStateAnimated = m_Data.arms[m_ArmState];
+            var armStateAnimated = m_PersonData.arms[m_ArmState];
             m_ArmRenderer.sprite = armStateAnimated[(int)(frame % armStateAnimated.Count)];
             m_ArmRenderer.flipX = (m_ReverseFlip)?  m_Direction.x < 0 : m_Direction.x > 0;
         }
         
         
-        void SetFaceState(AnimatedPersonData.FaceState faceState)
+        public void SetFaceState(AnimatedPersonData.FaceState faceState)
         {
             m_FaceState = faceState;
             UpdateRenderers(m_Frame);
--- GGJ2023/Assets/Vi Tests/View/Controller/AnimatedRideableController.cs
+++ GGJ2023/Assets/Vi Tests/View/Controller/AnimatedRideableController.cs
@@ -6,9 +6,9 @@ namespace Character
 {
     public class AnimatedRideableController : AnimatedCharacterController
     {
-        [SerializeField] AnimatedRideableData m_Data;
+        [SerializeField] AnimatedRideableData m_RideableData;
         [SerializeField] Transform m_RideablePoint;
         
-        internal override AnimatedCharacterData Data { get => m_Data; set => m_Data = (AnimatedRideableData)value; }
+        internal override AnimatedCharacterData Data { get => m_RideableData; set => m_RideableData = (AnimatedRideableData)value; }
     }
 }
